5fd6d7ee667247c726fcf5d4b613eae0593926cf
Schwellen001 <02schjul@rgto-me.it>
2020-04-23 11:33:46 +0200

    Main herrichten

--- MinirechnerTFO/MinirechnerTFO/Program.cs
+++ MinirechnerTFO/MinirechnerTFO/Program.cs
@@ -9,6 +9,19 @@ namespace MinirechnerTFO
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Minirechner");
+
+            Console.WriteLine("Zahl1: ");
+            int Zahl1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Zahl2: ");
+            int Zahl2 = Convert.ToInt32(Console.ReadLine());
+
+            
+
+
+
+
         }
     }
 }
